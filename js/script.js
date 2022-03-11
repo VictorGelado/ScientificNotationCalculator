79906bbcf21adb101da0a029ea47d5ac60e38303
VictorGelado <victorgeladogithub@gmail.com>
2022-03-11 16:43:00 -0300

    Finished calculator

--- js/script.js
+++ js/script.js
@@ -3,35 +3,50 @@ const submitButton = document.querySelector("#calc");
 const result = document.querySelector("#result");
 
 
-
 submitButton.addEventListener('click', calcNotation, false);
 
 function calcNotation(eve) {
   eve.preventDefault();
 
   let number = Number(numberInput.value);
-
-  let numberString = number.toString();
+  // let numberLength = number.toString().length - 1; 
 
   let resultNotation = '';
 
-  let div = numberString.length - 1; 
 
-  if (true) {
-    let numberNotation = number;
+    let numberNotation = number.toExponential();
 
-    /* 
-    for (let c = div; c >= 1; c--) {
+    let indexE = indexofE(numberNotation);
+
+    let exponent = numberNotation.indexOf('+') != -1? numberNotation.toString().substring(indexE).replace('+', ''): numberNotation.toString().substring(indexE);
+    
+    numberNotation = numberNotation.slice(0, indexE - 1);
+
+    resultNotation = `<p>${numberNotation} x 10<sup>${exponent}</sup></p>`;
+
+    result.innerHTML  = resultNotation.replace('.', ',');
+ 
+}
+
+function indexofE(numberNotation) {
+  let index = -1;
+  
+  if (numberNotation.indexOf('+') != -1) {
+    index = numberNotation.toString().indexOf('+');
+  } else if (numberNotation.indexOf('-') != -1) {
+    index = numberNotation.toString().indexOf('-');
+  }
+
+  return index;
+}
+
+
+
+
+
+/* 
+    for (let c = numberLength; c >= 1; c--) {
       numberNotation /= 10;
     } */
     
     // numberNotation = math.format(numberNotation,  {notation: 'fixed'});
-    
-    numberNotation = numberNotation.toExponential();
-
-    resultNotation = `<p>${numberNotation} x 10<sup>${div}</sup></p>`;
-
-    result.innerHTML  = resultNotation.replace('.', ',');
-  }
-}
-
